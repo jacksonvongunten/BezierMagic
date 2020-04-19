205adc7f7629dd71303a8205b4377c528dd3af82
Jackson VonGunten <jvongunten20@gmail.com>
2020-04-19 14:07:53 -0400

    Added a file containing the control points and ability to dynamically add and remove control points from the graph using the file. This should add for easier and faster development of paths. For now, this is a stand in for draggable points and a better GUI for more advanced control.

--- GeneralBezier.py
+++ GeneralBezier.py
@@ -17,6 +17,21 @@ class GeneralBezier:
     def add_control_points(self, points):
         for i in range(len(points)):
             self.p.insert(i+1, points[i])
+    
+    def remove_control_points(self):
+        del self.p[1:len(self.p)-2]
+    
+    def update_control_points(self, points):
+        self.remove_control_points()
+        self.add_control_points(points)
+    
+    def control_points_from_file(self, filename):
+        control_points = []
+        with open(filename, "r") as f:
+            for line in f.readlines():
+                point = line.rstrip().split(" ")
+                control_points.append(Point2D(float(point[0]), float(point[1])))
+        self.update_control_points(control_points)
 
     def get_points(self):
         return self.p
@@ -33,47 +48,7 @@ class GeneralBezier:
         self.lines = [GeneralBezier([self.p[i-1], self.p[i]]) for i in range(1, len(self.p))]
         return self.lines
     
-    def generate_x_velocity(self):
-        dt = 1/len(self.t)
-        x_derivatives = []
-        x_positions = self.generate_x()
-        for i in range(1, len(x_positions)):
-            x_derivatives.append((x_positions[i]-x_positions[i-1])/dt)
-        return x_derivatives
-    
-    def generate_y_velocity(self):
-        dt = 1/len(self.t)
-        y_derivatives = []
-        y_positions = self.generate_y()
-        for i in range(1, len(y_positions)):
-            y_derivatives.append((y_positions[i]-y_positions[i-1])/dt)
-        return y_derivatives
-    
-    def generate_velocity(self):
-        return [ sqrt(v[0]**2 + v[1]**2) for v in zip(self.generate_x_velocity(), self.generate_y_velocity()) ]
-
-    def generate_x_acceleration(self):
-        dt = 1/len(self.t)
-        x_derivatives = []
-        x_velocities = self.generate_x_velocity()
-        for i in range(1, len(x_velocities)):
-            x_derivatives.append((x_velocities[i]-x_velocities[i-1])/dt)
-        return x_derivatives
-    
-    def generate_y_acceleration(self):
-        dt = 1/len(self.t)
-        y_derivatives = []
-        y_velocities = self.generate_y_velocity()
-        for i in range(1, len(y_velocities)):
-            y_derivatives.append((y_velocities[i]-y_velocities[i-1])/dt)
-        return y_derivatives
-
-    def generate_acceleration(self):
-        return [ sqrt(a[0]**2 + a[1]**2) for a in zip(self.generate_x_acceleration(), self.generate_y_acceleration()) ]
-
     def plot(self):
-        velocities = self.generate_velocity()
-        accelerations = self.generate_acceleration()
         fig, ax = plt.subplots()
         plt.subplots_adjust(bottom=0.3)
         plt.grid()
@@ -82,27 +57,29 @@ class GeneralBezier:
         plt.ylabel("Y Position")
         xdata, ydata = [], []
         ln, = plt.plot([], [], color='k')
+        points, = plt.plot([], [], color='b', marker='x')
+        paths, = plt.plot([], [], color='b')
+
+        def draw_control_points():
+            self.control_points_from_file("control_points.txt")
+            control_point_x = [self.get_points()[i].get_x() for i in range(len(self.get_points()))]
+            points.set_xdata(control_point_x)
+            control_point_y = [self.get_points()[i].get_y() for i in range(len(self.get_points()))]
+            points.set_ydata(control_point_y)
+            paths.set_xdata([path.generate_x() for path in self.rough_path()])
+            paths.set_ydata([path.generate_y() for path in self.rough_path()])
+
+            ax.set_xlim(min(control_point_x)-1, max(control_point_x)+1)
+            ax.set_ylim(min(control_point_y)-1, max(control_point_y)+1)
 
         def update(t):
+            draw_control_points()
             xdata = self.generate_x()[:int(t*199)]
             ydata = self.generate_y()[:int(t*199)]
             ln.set_data(xdata, ydata)
-            velocity_slider.set_val(velocities[int(t*198)])
-            acceleration_slider.set_val(accelerations[int(t*197)])
-
-        for i in range(len(self.get_points())):
-            plt.plot(self.get_points()[i].get_x(), self.get_points()[i].get_y(), marker="x", color="b")
-        for path in self.rough_path():
-            plt.plot(path.generate_x(), path.generate_y(), color='b', linestyle="dashed")
 
         axslider1 = plt.axes([0.25, 0.15, 0.50, 0.02])
         time_slider = Slider(axslider1, "Time", 0, 1, 0)
-
-        axslider2 = plt.axes([0.25, 0.1, 0.50, 0.02])
-        velocity_slider = Slider(axslider2, "Velocity", 0, max(velocities), 0)
-
-        axslider3 = plt.axes([0.25, 0.05, 0.50, 0.02])
-        acceleration_slider = Slider(axslider3, "Acceleration", 0, max(accelerations), 0)
 
         time_slider.on_changed(update)
         
--- run.py
+++ run.py
@@ -3,8 +3,7 @@ from GeneralBezier import GeneralBezier
 
 if __name__ == "__main__":
     p = [Point2D(0,0), Point2D(5,5)]
-    control_points = [Point2D(1,8), Point2D(3, 10), Point2D(5,8)]
 
     B = GeneralBezier(p)
-    B.add_control_points(control_points)
+    B.control_points_from_file("control_points.txt")
     B.plot()
